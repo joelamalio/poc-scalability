04fb671a4395a9480924a9c8bb4ac69f6edeafce
Joel Amálio <joel.amalio@gmail.com>
2019-10-23 02:05:50 -0300

    add eureka project

---
+++ scalability-eureka/src/main/kotlin/br/com/joelamalio/scalabilityeureka/ScalabilityEurekaApplication.kt
@@ -0,0 +1,13 @@
+package br.com.joelamalio.scalabilityeureka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class ScalabilityEurekaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScalabilityEurekaApplication>(*args)
+}
